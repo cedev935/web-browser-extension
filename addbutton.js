a30169affd5dda103f51a012fb44ce5528634e19
Nathan Gerszanowilsz <fffan@free.fr>
2019-01-31 16:28:01 +0100

    WE: dev

--- addbutton.js
+++ addbutton.js
@@ -1,110 +1,95 @@
-console.log("coucou")
+
+console.log("Phantombuster Extension Loaded")
 const _browser = chrome || browser
-let websiteName, websiteUrl, cookiesName
+let websiteName, websiteUrl
 
-document.querySelectorAll(".launchButtonOptions, #launchButtonModalSwitchEditor").forEach(el => el.addEventListener("click", clickButton))
+const WebsiteEnum = {
+	LINKEDIN: {websiteName: "LinkedIn", websiteUrl: "https://www.linkedin.com/"},
+	TWITTER: {websiteName: "Twitter", websiteUrl: "https://twitter.com/"},
+	INSTAGRAM: {websiteName: "Instagram", websiteUrl: "https://www.instagram.com/"},
+	FACEBOOK: {websiteName: "Facebook", websiteUrl: "https://www.facebook.com/"}
+}
 
-function clickButton() {
-	var checkExist = setInterval(function() {
+// create the Get Cookies button
+const createButton = () => {
+	const checkExist = setInterval(() => {
 		if (document.querySelector("div[data-alpaca-field-path*=\"/sessionCookie\"] label a")) {
 			const apiLink = document.querySelector("div[data-alpaca-field-path*=\"/sessionCookie\"] label a").href
+			let website
 			if (apiLink.includes("/linkedin")) {
-				websiteName = "LinkedIn"
-				websiteUrl = "https://www.linkedin.com/"
-				cookiesName = ["li_at"]
+				website = "LINKEDIN"
 			} else if (apiLink.includes("/twitter")) {
-				websiteName = "Twitter"
-				websiteUrl = "https://twitter.com/"
-				cookiesName = ["auth_token"]
+				website = "TWITTER"
 			} else if (apiLink.includes("/instagram")) {
-				websiteName = "Instagram"
-				websiteUrl = "https://www.instagram.com/"
-				cookiesName = ["sessionid"]
+				website = "INSTAGRAM"
 			} else if (apiLink.includes("/facebook")) {
-				websiteName = "Facebook"
-				websiteUrl = "https://www.facebook.com/"
-				cookiesName = ["c_user", "xs"]
+				website = "FACEBOOK"
 			}
-			const btn = document.createElement("BUTTON")
+			websiteName = WebsiteEnum[website].websiteName
+			websiteUrl = WebsiteEnum[website].websiteUrl
 
-			// btn.setAttribute("href", websiteUrl)
-			// btn.setAttribute("target", "_blank")
+			const btn = document.createElement("BUTTON")
 			btn.textContent = `Get cookie${websiteName === "Facebook" ? "s" : ""} from ${websiteName}`
 			btn.id = "pbExtensionButton"
-			btn.style.color = "white"
-			btn.style.border = "none"
-			btn.style.borderRadius = "12px"
-			btn.style.backgroundColor ="#35C2DB"
-			btn.style.marginLeft = "5px"
-			btn.style.padding = "4px"
+			btn.classList.add("button-default")
 			btn.onclick = openConnection
 			if (!document.querySelector("#pbExtensionButton")) {
 				document.querySelector("div[data-alpaca-field-path*=\"/sessionCookie\"] label").appendChild(btn)
-				// if (document.querySelectorAll("div[data-alpaca-field-path*=\"/sessionCookie\"] label").length === 2) {
-				// 	document.querySelectorAll("div[data-alpaca-field-path*=\"/sessionCookie\"] label")[1].appendChild(btn.cloneNode(true))
-				// }
 			}
 		   clearInterval(checkExist)
 		}
-	 }, 100)
+	}, 100)
 }
 
+// send a message to background script
+const sendMessage = (message) => {
+	_browser.runtime.sendMessage(message)
+}
 
-function openConnection() {
+// send the website name to background to query its cookies
+const openConnection = () => {
 	console.log("opening Connection")
-	var port = _browser.runtime.connect({name: "phantombusterCookie"});
-	port.postMessage({websiteName, cookiesName});
-	port.onMessage.addListener(function(msg) {
-		console.log("Message received: ", msg)
-		if (msg.retrievedCookies) {
-			const cookies = msg.retrievedCookies
-			// console.log("retrieved cookie1 = ", cookies)
-			console.log("retrieved cookie3= ", cookies[0])
-			if (cookies[0]) {
-				setCookies(cookies)
-			} else {
-				document.querySelectorAll("#pbExtensionButton").forEach(el => el.style.backgroundColor ="#8B0000")
-				window.open(websiteUrl, '_blank')
-				port.postMessage({opening: websiteName})
-			}
-		}
-		if (msg.opened) {
-			console.log("We've opened the page")
-		}
-		// if (msg.okies) {
-		// 	for (let cookie of msg.okies) {
-		// 		console.log("cookie: ", cookie)
-		// 	}
-		// }
-	})
-
+	sendMessage({websiteName})
 }
 
+// fill the form with the correct cookie(s)
 const setCookies = (cookies) => {
 	document.querySelector("div[data-alpaca-field-path*=\"/sessionCookie\"] input").value = cookies[0].value
 	if (websiteName === "Facebook") {
 		document.querySelectorAll("div[data-alpaca-field-path*=\"/sessionCookie\"] input")[1].value = cookies[1].value
 	}
-	document.querySelectorAll("#pbExtensionButton").forEach(el => el.style.backgroundColor ="#00b200")
+	document.querySelectorAll("#pbExtensionButton").forEach(el => {
+		el.classList.add("button-success")
+		if (el.classList.contains("button-failure")) {
+			el.classList.remove("button-failure")
+		}
+		el.textContent = `${websiteName} Cookie${websiteName === "Facebook" ? "s" : ""} successfully pasted!`
+	})
 }
 
-_browser.runtime.onMessage.addListener(
-	function(cookies, sender, sendResponse) {
-		console.log("newCookies", cookies)
-		setCookies(cookies.retrievedCookies)
-	})
+// listen to messages from background
+_browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
+	console.log("Message received:", message)
+	if (message.opened) {
+		console.log("We've opened the page.")
+	}
+	if (message.cookies) {
+		const cookies = message.cookies
+		if (cookies[0]) {
+			console.log("Received cookies:", cookies[0])
+			setCookies(cookies)
+		} else {
+			console.log("No cookies found!")
+			document.querySelectorAll("#pbExtensionButton").forEach(el => {
+				el.classList.replace("button-default", "button-failure")
+				el.textContent = `Please log in to ${websiteName} to get your cookie`
+			})
+			window.open(websiteUrl, '_blank')
+			sendMessage({opening: websiteName})
+		}
+	}
+})
 
-// function setCookies (cookies) {
-// 	console.log("retrieved cookie3= ", cookies[0])
-// 	if (cookies[0]) {
-// 		document.querySelector("div[data-alpaca-field-path*=\"/sessionCookie\"] input").value = cookies[0].value
-// 		if (websiteName === "Facebook") {
-// 			document.querySelectorAll("div[data-alpaca-field-path*=\"/sessionCookie\"] input")[1].value = cookies[1].value
-// 		}
-// 		document.querySelectorAll("#pbExtensionButton").forEach(el => el.style.backgroundColor ="#00b200")
-// 	} else {
-// 		document.querySelectorAll("#pbExtensionButton").forEach(el => el.style.backgroundColor ="#8B0000")
-// 		window.open(websiteUrl, '_blank');
-// 		port.postMessage({opening: websiteName})
-// 	}
-// }
+// add an event listener next to all launch buttons
+document.querySelectorAll(".launchButtonOptions, #launchButtonModalSwitchEditor").forEach(el => el.addEventListener("click", createButton))
+
--- background.js
+++ background.js
@@ -1,165 +1,48 @@
 // background-script.js
 "use strict";
-// console.log("window:", window)
 const _browser = chrome || browser
-let domain, cookiesName
-let retrievedCookies
+let domain, cookiesList
 let tabID
-// function onError(error) {
-//   console.error(`Error: ${error}`);
-// }
 
-// function sendMessageToTabs(tabs) {
-//   for (let tab of tabs) {
-//     chrome.tabs.sendMessage(
-//       tab.id,
-//       {greeting: "Hi from background script"}
-//     ).then(response => {
-//       console.log("Message from the content script:");
-//       console.log(response.response);
-//     }).catch(onError);
-//   }
-// }
+const CookiesListEnum = {
+	LinkedIn: {domain: ".linkedin.com", cookiesList: ["li_at"]},
+	Twitter: {domain: ".twitter.com", cookiesList: ["auth_token"]},
+	Instagram: {domain: ".instagram.com", cookiesList: ["sessionid"]},
+	Facebook: {domain: ".facebook.com", cookiesList: ["c_user", "xs"]}
+}
 
-// _browser.browserAction.onClicked.addListener(() => {
-//   _browser.tabs.query({
-//     currentWindow: true,
-//     active: true
-//   }).then(sendMessageToTabs).catch(onError);
-// });
+const sendCookie = (cookies) => {
+	_browser.tabs.sendMessage(tabID, {cookies})
+}
 
-// function handleClick() {
-//     console.log("do something.");
-//     // If you want to something with the content, you will need a content script and messaging
-// }
-
-// _browser.browserAction.onClicked.addListener(handleClick);
-
-_browser.runtime.onConnect.addListener(connected)
-
-function _getCurrentTab(callback){ //Take a callback
-    _browser.tabs.query({active:true, currentWindow:true},function(tab){
-        callback(tab); //call the callback with argument
-    });
-};
-
-function displayTab(tab) { //define your callback function
-	console.log("currentTab:", tab);
-	return tab
- };
-
-//  _getCurrentTab(_displayTab); //invoke the function with the callback function reference
-
-function connected(port) {
-	console.log("listening connection")
-	console.assert(port.name == "phantombusterCookie");
-	port.onMessage.addListener(function(msg) {
-		console.log("msgbg:", msg)
-		
-		if (msg.websiteName) {
-			const websiteName = msg.websiteName
-			cookiesName = msg.cookiesName
-			_browser.tabs.query({
-				active: true,
-				currentWindow: true
-			}, function (currentTab) {
-				tabID = currentTab[0].id
-				console.log("tabID = ", tabID)
-		})
-			console.log("retrieving cookie for ", websiteName)
-			switch(websiteName) {
-				case "LinkedIn":
-					domain = ".twitter.com"
-					break
-				case "Facebook":
-					domain = ".facebook.com"
-					break
-				case "Twitter":
-					domain = ".twitter.com"
-					break
-				case "Instagram":
-					domain = ".instagram.com"
-					break
-			}
-			_browser.cookies.getAll({
-				domain
-			  }, function (cookies) {
-					retrievedCookies = cookiesName.map(name => cookies.filter(el => el.name === name)[0])
-					port.postMessage({retrievedCookies})
-			  })
-		}
-		if (msg.opening) {
-			_browser.cookies.onChanged.addListener(cookieChanged)
-			port.postMessage({opened: true})
+const cookieChanged = (changeInfo) => {
+	_browser.cookies.getAll({ domain }, (cookies) => {
+		const retrievedCookies = cookiesList.map(name => cookies.filter(el => el.name === name)[0])
+		if (retrievedCookies[0]) {
+			console.log("retrievedCookiesChanged", retrievedCookies[0])
+			console.log("tabID is still", tabID)
+			_browser.cookies.onChanged.removeListener(cookieChanged)
+			sendCookie(retrievedCookies)
 		}
 	})
 }
 
-function promiseQuery(options){
-	return new Promise(function(resolve,reject){
-	  _browser.tabs.query(options, resolve);
-	});
-}
-
-function cookieChanged(changeInfo) {
-	// console.log("cookie changed:", changeInfo)
-	// console.log("domain Changed:", domain)
-	_browser.cookies.getAll({
-		domain
-	  }, async function (cookies) {
-		  retrievedCookies = cookiesName.map(name => cookies.filter(el => el.name === name)[0])
-		  if (retrievedCookies[0]) {
-			console.log("retrievedCookiesChanged", retrievedCookies[0])
-			console.log("tabID is still", tabID)
-			_browser.cookies.onChanged.removeListener(cookieChanged)
-			const allTabs = await promiseQuery({}).then(function (tabs) {
-				return tabs
-			})
-			console.log("alltab:", allTabs)
-			const pbTab = allTabs.filter(el => el.id === tabID)
-			console.log("pbtab:", pbTab)
-			// _browser.tabs.query({active: true, currentWindow: true}, function(tabs) {
-			_browser.tabs.sendMessage(tabID, {retrievedCookies}, function(response) {
-				  console.log("rsvp");
-				})
-			//   })
-			// _browser.runtime.sendMessage({retrievedCookies})
-		}
-		// port.postMessage({retrievedCookies});
-		// port.postMessage({allCookies: cookies})
-	  })
-	// _browser.cookies.getAll({
-	// 	evt.target.domain
-	//   }, function (cookies) {
-	// 	  const retrievedCookies = cookiesName.map(name => cookies.filter(el => el.name === name)[0])
-	// 	for (var i = 0; i < cookies.length; i++) {
-	// 	  console.log("bgC:", cookies[i])
-	// 	}
-	//   })
-}
-// var dataTempStorage = [];
-
-// _browser.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-// 	console.log("mkt1", request)
-// 	chrome.cookies.getAll({
-// 		domain: ".twitter.com"
-// 	  }, function (cookies) {
-// 		for (var i = 0; i < cookies.length; i++) {
-// 		  console.log(cookies[i])
-// 		}
-// 	  });
-//     if (request.setdata) {
-//         dataTempStorage = request.setdata;
-// 		console.log("mkt2")
-
-//         _browser.tabs.create({
-//             'url': _browser.extension.getURL('newpage.html')
-//         })
-//     }
-
-//     if (request == "getdata") {
-// 		sendResponse({data: dataTempStorage});
-// 		console.log("mkt3")
-
-// 	}
-// })
+_browser.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+	if (msg.opening) {
+		_browser.cookies.onChanged.addListener(cookieChanged)
+	}
+	if (msg.websiteName) {
+		const websiteName = msg.websiteName
+		_browser.tabs.query({ active: true, currentWindow: true }, (currentTab) => {
+				tabID = currentTab[0].id
+				console.log("tabID = ", tabID)
+		})
+		domain = CookiesListEnum[websiteName].domain
+		cookiesList = CookiesListEnum[websiteName].cookiesList
+		_browser.cookies.getAll({ domain }, (cookies) => {
+			const retrievedCookies = cookiesList.map(name => cookies.filter(el => el.name === name)[0])
+			console.log("retrivedCookiesbeforesending:", retrievedCookies)
+			sendCookie(retrievedCookies)
+		})
+	}
+})
