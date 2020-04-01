52e3d61b31ba1529eeb3be513ec95817bf433b3a
Maxime Leduc <maxime.leduc@phantombuster.com>
2020-04-01 17:06:28 +0200

    Improvements to popup

--- src/popup/src/App.tsx
+++ src/popup/src/App.tsx
@@ -1,24 +1,58 @@
-import React from "react"
+import React, { FunctionComponent } from "react"
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route
+} from "react-router-dom"
+import "pb2-css/dist/less/style.css"
 import "./App.css"
 
-function App() {
+const _browserMain = chrome || browser
+
+interface AProps {
+	className: string
+	href: string
+}
+
+const A: FunctionComponent<AProps> = ({children, className, href}) => {
+	const click = (event: React.MouseEvent<HTMLAnchorElement>) => {
+		event.preventDefault()
+		_browserMain.tabs.create({url: href})
+		window.close()
+	}
+
 	return (
-		<div className="App">
-			<header className="App-header">
+		<a className={className} href={href} target="_blank" rel="noopener noreferrer" onClick={click}>
+			{children}
+		</a>
+	)
+}
+
+const Home: FunctionComponent = () => {
+	return (
+		<div className="flex">
+			<header className="w-full">
 				<ul>
-					<li>
-						<a
-							className="App-link"
-							href="https://phantombuster.com"
-							target="_blank"
-							rel="noopener noreferrer"
-							>
+					<li className="text-align-center">
+						<A className="block w-full py-3 hover:bg-grey-5" href="https://phantombuster.com">
 							Open Phantombuster
-						</a>
+						</A>
 					</li>
 				</ul>
 			</header>
 		</div>
+	)
+}
+
+const App: FunctionComponent = () => {
+	return (
+		<Router>
+			<Switch>
+				<Route path="/" component={Home}/>
+				<Route path="/popup.html" component={Home} />
+				<Route path="/window.html" component={Home} />
+			</Switch>
+		</Router>
 	)
 }
 
