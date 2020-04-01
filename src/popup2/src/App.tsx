706cf621c50b7255c76b28f30ea05de532efc4e8
Maxime Leduc <maxime.leduc@phantombuster.com>
2020-04-01 11:56:37 +0200

    Hack renaming 1/2

---
+++ src/popup2/src/App.tsx
@@ -0,0 +1,25 @@
+import React from "react"
+import "./App.css"
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<ul>
+					<li>
+						<a
+							className="App-link"
+							href="https://phantombuster.com"
+							target="_blank"
+							rel="noopener noreferrer"
+							>
+							Open Phantombuster
+						</a>
+					</li>
+				</ul>
+			</header>
+		</div>
+	)
+}
+
+export default App
