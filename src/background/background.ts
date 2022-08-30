7f88e629f39fa5ce1adfec14ff0c8d73576df852
cedev935 <cedev935@gmail.com>
2022-08-30 10:36:43 +0200

    fea(sentry): setup sentry for background script

--- src/background/background.ts
+++ src/background/background.ts
@@ -2,9 +2,22 @@ import { extensionWebsiteDomains, WebsiteName, getWebsiteFromName } from "../sha
 import { processCookieStr, processSetCookieStr } from "../shared/cookies"
 import { browser, Tabs, WebRequest, Runtime, Cookies } from "webextension-polyfill-ts"
 import { FromContentScriptRuntimeMessages, FromBackgroundRuntimeMessages } from "../shared/messages"
+import * as Sentry from "@sentry/browser"
+import { version } from "../../manifest.json"
+
+Sentry.init({
+	dsn: "https://a4bfab3486a647ea94cab5580874e008@o303567.ingest.sentry.io/6698508",
+	release: version,
+})
 
 const isChrome = () => {
 	return document.location.protocol.indexOf("chrome") !== -1
+}
+
+const captureRuntimeErrorIfAny = () => {
+	if (browser.runtime.lastError) {
+		Sentry.captureException(browser.runtime.lastError)
+	}
 }
 
 // Only global here.
@@ -41,49 +54,59 @@ const sendMessage = async (tabId: number, msg: FromBackgroundRuntimeMessages) =>
 // Function to be used to send notifications instead of browser.notifications.create()
 const sendNotification = (title: string, message: string) => {
 	// tslint:disable-next-line:ban
-	browser.notifications
-		.create({ type: "basic", message, title, iconUrl: "assets/buster-icon-48.png" })
-		.catch((error: Error) => {
-			console.error(error)
-		})
+	browser.notifications.create({ type: "basic", message, title, iconUrl: "assets/buster-icon-48.png" })
 }
 
 // Here we attach an a listener to each tab url change to (re)start the extension if the domain matches
 // the list of domains where we want the extension to run content scripts.
 browser.tabs.onUpdated.addListener(async (id, changeInfo, tab) => {
-	if (tab.url && extensionWebsiteDomains.some((v) => tab.url!.includes(v)) && changeInfo.status === "complete") {
-		await sendMessage(id, { restart: true })
+	captureRuntimeErrorIfAny()
+	try {
+		if (tab.url && extensionWebsiteDomains.some((v) => tab.url!.includes(v)) && changeInfo.status === "complete") {
+			await sendMessage(id, { restart: true })
+		}
+	} catch (error) {
+		Sentry.captureException(error)
 	}
 })
 
 // At the extension installation or update (or browser update) we reload/restart on each tab matching the list
 // of domains where we want the extension to run content scripts
 browser.runtime.onInstalled.addListener(async () => {
-	const tabs = await browser.tabs.query({ url: extensionWebsiteDomains.map((url) => `*://*.${url}/*`) })
-	for (const t of tabs) {
-		if (t.id) {
-			if (isChrome()) {
-				// Google chrome does not (re)install content scripts so we need to do a full reload of the tab
-				await browser.tabs.reload(t.id)
-			} else {
-				// Firefox (re)installs content scripts directly so we just need to send the "restart" message
-				await sendMessage(t.id, { restart: true })
+	captureRuntimeErrorIfAny()
+	try {
+		const tabs = await browser.tabs.query({ url: extensionWebsiteDomains.map((url) => `*://*.${url}/*`) })
+		for (const t of tabs) {
+			if (t.id) {
+				if (isChrome()) {
+					// Google chrome does not (re)install content scripts so we need to do a full reload of the tab
+					await browser.tabs.reload(t.id)
+				} else {
+					// Firefox (re)installs content scripts directly so we just need to send the "restart" message
+					await sendMessage(t.id, { restart: true })
+				}
 			}
 		}
+	} catch (error) {
+		Sentry.captureException(error)
 	}
 })
 
 // Here we receive messages from the content scripts
 browser.runtime.onMessage.addListener(async (msg: FromContentScriptRuntimeMessages, sender: Runtime.MessageSender) => {
-	// console.log("Message received", msg)
-	if (msg.newTab && sender.tab) {
-		await newTab(msg.newTab.websiteName, msg.newTab.url, msg.newTab.newSession, sender.tab)
-	} else if (msg.getCookies && sender.tab) {
-		await getCookies(msg.getCookies.websiteName, msg.getCookies.newSession, sender.tab)
-	} else if (msg.notif) {
-		sendNotification(msg.notif.title || "PhantomBuster", msg.notif.message)
-	} else if (msg.restartMe && sender.tab && sender.tab.id) {
-		await sendMessage(sender.tab.id, { restart: true })
+	captureRuntimeErrorIfAny()
+	try {
+		if (msg.newTab && sender.tab) {
+			await newTab(msg.newTab.websiteName, msg.newTab.url, msg.newTab.newSession, sender.tab)
+		} else if (msg.getCookies && sender.tab) {
+			await getCookies(msg.getCookies.websiteName, msg.getCookies.newSession, sender.tab)
+		} else if (msg.notif) {
+			sendNotification(msg.notif.title || "PhantomBuster", msg.notif.message)
+		} else if (msg.restartMe && sender.tab && sender.tab.id) {
+			await sendMessage(sender.tab.id, { restart: true })
+		}
+	} catch (error) {
+		Sentry.captureException(error)
 	}
 })
 
@@ -246,5 +269,6 @@ const cookieChanged = async (
 
 // opens phantombuster in a new tab when clicking on the extension icon
 browser.browserAction.onClicked.addListener(async (_tab) => {
+	captureRuntimeErrorIfAny()
 	await browser.tabs.create({ url: "https://phantombuster.com/phantoms" })
 })
