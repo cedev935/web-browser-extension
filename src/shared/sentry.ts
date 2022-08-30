b8ecb361c6968c389d7800d6539c549fb116732d
cedev935 <cedev935@gmail.com>
2022-08-30 15:15:41 +0200

    feat(sentry): add shared sentry functions

---
+++ src/shared/sentry.ts
@@ -0,0 +1,42 @@
+import * as Sentry from "@sentry/browser"
+import { browser } from "webextension-polyfill-ts"
+import { version } from "../../manifest.json"
+
+export function initSentry() {
+	Sentry.init({
+		dsn: "https://a4bfab3486a647ea94cab5580874e008@o303567.ingest.sentry.io/6698508",
+		release: version,
+	})
+}
+
+export function captureRuntimeErrorIfAny() {
+	if (browser.runtime.lastError) {
+		Sentry.captureException(browser.runtime.lastError)
+	}
+}
+
+export function wrapAsyncFunctionWithSentry<F extends (...args: any[]) => Promise<unknown>>(asyncFunction: F): F {
+	return (async (...args: Parameters<F>) => {
+		captureRuntimeErrorIfAny()
+		try {
+			const result = await asyncFunction(...args)
+			return result
+		} catch (error) {
+			Sentry.captureException(error)
+			throw error
+		}
+	}) as F
+}
+
+export function wrapFunctionWithSentry<F extends (...args: any[]) => unknown>(func: F): F {
+	return ((...args: Parameters<F>) => {
+		captureRuntimeErrorIfAny()
+		try {
+			const result = func(...args)
+			return result
+		} catch (error) {
+			Sentry.captureException(error)
+			throw error
+		}
+	}) as F
+}
