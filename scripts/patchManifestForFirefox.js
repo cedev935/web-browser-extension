d023dcaee3b0b128242c5e2627cdb38ef6ab408f
Gabriel Cousin <7649529+GabrielCousin@users.noreply.github.com>
2023-03-06 16:06:57 +0100

    chore(manifest): update webpack to build mv2 or mv3

--- scripts/patchManifestForFirefox.js
+++ scripts/patchManifestForFirefox.js
@@ -7,10 +7,24 @@ const filePaths = process.argv.filter((path) => /.json$/i.test(path))
 filePaths.forEach((path) => {
 	const obj = JSON.parse(fs.readFileSync(path))
 
+	// set to version 2
+	obj.manifest_version = 2
+
+	// revert `service_worker` into `background`
 	obj.background = {
 		scripts: [obj.background.service_worker],
 	}
 
-	console.log("Patching MV3 for Firefox:", path)
+	// revert `browser_action` into `action`
+	obj.action = obj.browser_action
+	delete obj.browser_action
+
+	// add back `web_accessible_resources`
+	obj.web_accessible_resources = ["assets/*"]
+
+	// revert `host_permissions` into `permissions`
+	obj.permissions.push(...obj.host_permissions)
+	delete obj.host_permissions
+
 	fs.writeFileSync(path, JSON.stringify(obj, null, 2))
 })
